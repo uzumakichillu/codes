81eae660a412776d52ad998521364fc2e42fa0f7
uzumakichillu <34211759+uzumakichillu@users.noreply.github.com>
2019-07-04 01:42:53 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,9 @@
+# set is collection of unique entries
+ st={'apple','mango','orange'}
+ print(st)
+ st.add('banana')
+ print('mango' in st)
+ print('mangos' in st)
+ st.remove('orange')
+ st.clear()
+ del st
